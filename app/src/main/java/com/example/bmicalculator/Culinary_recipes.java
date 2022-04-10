2ec30383f14c7ab22af63c66c0ebbcc5fd64e5f6
Kaczmarek-M <m.kaczmarek1284@gmail.com>
2022-04-10 20:48:22 +0200

    Improved Scripts

--- app/src/main/java/com/example/bmicalculator/Culinary_recipes.java
+++ app/src/main/java/com/example/bmicalculator/Culinary_recipes.java
@@ -3,6 +3,7 @@ package com.example.bmicalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +17,20 @@ public class Culinary_recipes extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_culinary_recipes);
 
+        findViewById(R.id.firstRec).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firstRecipe("https://recipes.net/main-dish/vegetables/asparagus-with-sherry-and-bacon-vinaigrette-recipe/#wprm-recipe-container-261");
+            }
+        });
+
+        findViewById(R.id.secondRec).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                secondRecipe("https://recipes.net/main-dish/pasta/ravioli-alfredo-with-herbes-de-provence-recipe");
+            }
+        });
+
         back = (Button) findViewById(R.id.back);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,5 +39,18 @@ public class Culinary_recipes extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+    }
+    public void firstRecipe (String url){
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+    }
+    public void secondRecipe (String url){
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
     }
 }
